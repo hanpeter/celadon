5c87fca9f709aaedfa02ce122711121e069b50ac
Peter Han <git@peterhan.ca>
2026-05-28 00:25:38 -0700

    feat(frontend): Build output to celadon/static for Flask to serve

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,24 +5,13 @@ export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
+    outDir: '../celadon/static',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: 'index.html',
         login: 'login.html',
       },
-    },
-  },
-  server: {
-    port: 9001,
-    proxy: {
-      '/customer': 'http://localhost:9002',
-      '/sale': 'http://localhost:9002',
-      '/purchaser': 'http://localhost:9002',
-      '/purchase': 'http://localhost:9002',
-      '/item': 'http://localhost:9002',
-      '/auth': 'http://localhost:9002',
-      '/login': 'http://localhost:9002',
-      '/logout': 'http://localhost:9002',
     },
   },
 });
